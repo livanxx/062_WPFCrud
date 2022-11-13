911e61fe3911347ddfad4cb6fcc91e7c0a19b0f2
Livanx QM <livanxx@hotmail.com>
2022-11-13 12:27:16 -0600

    Agregar archivos de proyecto.

---
+++ 062_WPFCrud/Formulario.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _062_WPFCrud
+{
+    /// <summary>
+    /// Lógica de interacción para Formulario.xaml
+    /// </summary>
+    public partial class Formulario : Page
+    {
+        public int Id = 0;
+        public Formulario(int Id = 0)
+        {
+            InitializeComponent();
+            this.Id = Id;
+            if (this.Id != 0) 
+            {
+                using (Model.WPFCrudEntities db = new Model.WPFCrudEntities()) 
+                {
+                    var oPerson = db.person.Find(this.Id);
+                    txtEdad.Text = oPerson.Age.ToString();
+                    txtNombre.Text = oPerson.Name;   
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Id == 0)
+            {
+                using (Model.WPFCrudEntities db = new Model.WPFCrudEntities())
+                {
+                    var oPerson = new Model.person();
+                    oPerson.Name = txtNombre.Text;
+                    oPerson.Age = int.Parse(txtEdad.Text);
+
+                    db.person.Add(oPerson);
+                    db.SaveChanges();
+
+                    MainWindow.StaticMainFrame.Content = new MenuYLista();
+                }
+            }
+            else 
+            {
+                using (Model.WPFCrudEntities db = new Model.WPFCrudEntities())
+                {
+                    var oPerson = db.person.Find(Id);
+                    oPerson.Name = txtNombre.Text;
+                    oPerson.Age = int.Parse(txtEdad.Text);
+
+                    db.Entry(oPerson).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+
+                    MainWindow.StaticMainFrame.Content = new MenuYLista();
+                }
+            }
+        }
+    }
+}
---
+++ 062_WPFCrud/MenuYLista.xaml.cs
@@ -0,0 +1,83 @@
+﻿using _062_WPFCrud.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _062_WPFCrud
+{
+    /// <summary>
+    /// Lógica de interacción para MenuYLista.xaml
+    /// </summary>
+    public partial class MenuYLista : Page
+    {
+        public MenuYLista()
+        {
+            InitializeComponent();
+            Refresh();
+        }
+
+        private void Refresh() 
+        {
+            List<PersonViewModel> lst = new List<PersonViewModel>();
+            using (Model.WPFCrudEntities db = new Model.WPFCrudEntities()) 
+            {
+                lst = (from d in db.person
+                       select new PersonViewModel
+                       {
+                           Name = d.Name,
+                           Age = d.Age,
+                           Id= d.Id
+                       }).ToList();
+            }
+
+            DG.ItemsSource = lst;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.StaticMainFrame.Content = new Formulario();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int Id = (int)((Button)sender).CommandParameter;
+
+            using (Model.WPFCrudEntities db = new Model.WPFCrudEntities()) 
+            {
+                var oPerson = db.person.Find(Id);
+
+                db.person.Remove(oPerson);
+                db.SaveChanges();
+            }
+            Refresh();
+        }
+
+        private void Button_Editar(object sender, RoutedEventArgs e)
+        {
+            int Id = (int)((Button)sender).CommandParameter;
+
+            Formulario pFormulario = new Formulario(Id);
+
+            MainWindow.StaticMainFrame.Content = pFormulario;
+        }
+    }
+
+    public class PersonViewModel 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Age { get; set; }
+    }
+}
